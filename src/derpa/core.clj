53cd19225e64b7d8ee19e35612688ccba4f113c0
Reefersleep <soerenknud@gmail.com>
2015-06-02 01:18:21 +0200

    Separate continuous interpretation and steps
    Refactors 'interpret' function, which previously recursed over the
    app-state as well as transforming it, into a 'step' function which
    performs a single-step transformation and an 'interpret' function
    which takes care of the recursing, calling 'step' until reaching
    the termination condition.

--- src/derpa/core.clj
+++ src/derpa/core.clj
@@ -3,7 +3,7 @@
 
 (defn initialize-cells [number] (vec (repeat number 0)))
 
-(defn out-of-sourcecode-bounds? [app-state]
+(defn out-of-upper-sourcecode-bounds? [app-state]
   (= (:reader-position app-state) (count (:src app-state)))) ;;TODO currently only checks for upper bounds
 
 (defn retrieve-current-symbol [app-state]
@@ -18,6 +18,105 @@
 (defn looping-backward? [app-state]
   (= :looping-backward (:movement app-state)))
 
+(defn step [app-state]
+  (let [{:keys [src 
+                reader-position 
+                cells 
+                cell-pointer 
+                movement 
+                nested-left-brackets 
+                nested-right-brackets]} app-state]
+    (if (out-of-upper-sourcecode-bounds? app-state) (assoc app-state :terminated-due-to :reached-upper-sourcecode-bounds)
+      (let [current-symbol (retrieve-current-symbol app-state)] ;; Read the current symbol
+        (cond
+          (looping-forward? app-state) (if (= \] current-symbol)
+                                         (if (brackets-balanced? app-state) 
+                                           ;; stop looping and start moving forward normally - reset nested brackets
+                                           (assoc app-state
+                                                  :reader-position (inc reader-position)
+                                                  :movement :moving-forward
+                                                  :nested-left-brackets 0
+                                                  :nested-right-brackets 0)
+                                           ;; continue looping and add nested right bracket
+                                           (assoc app-state 
+                                                  :reader-position (inc reader-position)
+                                                  :movement :looping-forward
+                                                  :nested-right-brackets (inc nested-right-brackets)))
+                                         (if (= \[ current-symbol)
+                                           ;; continue looping and add nested left bracket
+                                           (assoc app-state
+                                                  :reader-position (inc reader-position)
+                                                  :movement :looping-forward
+                                                  :nested-left-brackets (inc nested-left-brackets))
+                                           ;; continue looping
+                                           (assoc app-state 
+                                                  :reader-position (inc reader-position)
+                                                  :movement :looping-forward)))
+          (looping-backward? app-state) (if (= \[ current-symbol)
+                                          (if (brackets-balanced? app-state)
+                                            ;; stop looping and start moving forward normally - reset nested brackets
+                                            (assoc app-state
+                                                   :reader-position (inc reader-position) 
+                                                   :movement :moving-forward
+                                                   :nested-left-brackets 0
+                                                   :nested-right-brackets 0)
+                                            ;; continue looping and add nested left bracket
+                                            (assoc app-state 
+                                                   :reader-position (dec reader-position)
+                                                   :movement :looping-backward
+                                                   :nested-left-brackets (inc nested-left-brackets)))
+                                          (if (= \] current-symbol)
+                                            ;; continue looping and add nested right bracket
+                                            (assoc app-state 
+                                                   :reader-position (dec reader-position)
+                                                   :movement :looping-backward
+                                                   :nested-right-brackets (inc nested-right-brackets))
+                                            ;; continue looping 
+                                            (assoc app-state 
+                                                   :reader-position (dec reader-position)
+                                                   :movement :looping-backward)))
+          (= \+ current-symbol) (assoc app-state
+                                       :reader-position (inc reader-position)
+                                       :cells (assoc cells cell-pointer (inc (nth cells cell-pointer))) 
+                                       :movement :moving-forward)
+          (= \- current-symbol) (assoc app-state
+                                       :reader-position (inc reader-position)
+                                       :cells (assoc cells cell-pointer (dec (nth cells cell-pointer))) 
+                                       :movement :moving-forward)
+          (= \> current-symbol) (assoc app-state 
+                                       :reader-position (inc reader-position)
+                                       :cell-pointer (inc cell-pointer)
+                                       :movement :moving-forward)
+          (= \< current-symbol) (assoc app-state
+                                       :reader-position (inc reader-position)
+                                       :cell-pointer (dec cell-pointer)
+                                       :movement :moving-forward)
+          (= \. current-symbol) (do (print (char (nth cells cell-pointer)))
+                                    (assoc app-state
+                                           :reader-position (inc reader-position)
+                                           :movement :moving-forward))
+          (= \[ current-symbol) (if (= 0 (nth cells cell-pointer)) 
+                                  (assoc app-state
+                                         :reader-position (inc reader-position)
+                                         :movement :looping-forward)
+                                  (assoc app-state
+                                         :reader-position (inc reader-position)
+                                         :movement :moving-forward))
+          (= \] current-symbol) (if (not (= 0 (nth cells cell-pointer)))
+                                  (assoc app-state
+                                         :reader-position (dec reader-position)
+                                         :movement :looping-backward)
+                                  (assoc app-state
+                                         :reader-position (inc reader-position)
+                                         :movement :moving-forward))
+          (= \, current-symbol) (assoc app-state
+                                       :reader-position (inc reader-position)
+                                       :cells (assoc cells cell-pointer (int (first (read-line))))
+                                       :movement :moving-forward)
+          :else (assoc app-state
+                       :reader-position (inc reader-position)
+                       :movement :moving-forward)))))) ;; Moves reader-position forward if the current character is unknown
+
 (defn interpret [src]
   (let [app-state {:src src
                    :reader-position 0
@@ -25,98 +124,12 @@
                    :cell-pointer 50
                    :movement :moving-forward
                    :nested-left-brackets 0
-                   :nested-right-brackets 0}]
+                   :nested-right-brackets 0
+                   :terminated-due-to nil}]
     (loop [app-state app-state]
-      (if (out-of-sourcecode-bounds? app-state) app-state ;; Terminal case - stop reading
-        (let [current-symbol (retrieve-current-symbol app-state)] ;; Read the current symbol
-          (cond
-            (looping-forward? app-state) (if (= \] current-symbol)
-                                           (if (brackets-balanced? app-state) 
-                                             ;; stop looping and start moving forward normally - reset nested brackets
-                                             (recur (assoc app-state
-                                                           :reader-position (inc (:reader-position app-state))
-                                                           :movement :moving-forward
-                                                           :nested-left-brackets 0
-                                                           :nested-right-brackets 0))
-                                             ;; continue looping and add nested right bracket
-                                             (recur (assoc app-state 
-                                                           :reader-position (inc (:reader-position app-state))
-                                                           :movement :looping-forward
-                                                           :nested-right-brackets (inc (:nested-right-brackets app-state)))))
-                                           (if (= \[ current-symbol)
-                                             ;; continue looping and add nested left bracket
-                                             (recur (assoc app-state
-                                                           :reader-position (inc (:reader-position app-state))
-                                                           :movement :looping-forward
-                                                           :nested-left-brackets (inc (:nested-left-brackets app-state))))
-                                             ;; continue looping
-                                             (recur (assoc app-state 
-                                                           :reader-position (inc (:reader-position app-state))
-                                                           :movement :looping-forward))))
-            (looping-backward? app-state) (if (= \[ current-symbol)
-                                            (if (brackets-balanced? app-state)
-                                              ;; stop looping and start moving forward normally - reset nested brackets
-                                              (recur (assoc app-state
-                                                            :reader-position (inc (:reader-position app-state)) 
-                                                            :movement :moving-forward
-                                                            :nested-left-brackets 0
-                                                            :nested-right-brackets 0))
-                                              ;; continue looping and add nested left bracket
-                                              (recur (assoc app-state 
-                                                            :reader-position (dec (:reader-position app-state))
-                                                            :movement :looping-backward
-                                                            :nested-left-brackets (inc (:nested-left-brackets app-state)))))
-                                            (if (= \] current-symbol)
-                                              ;; continue looping and add nested right bracket
-                                              (recur (assoc app-state 
-                                                            :reader-position (dec (:reader-position app-state))
-                                                            :movement :looping-backward
-                                                            :nested-right-brackets (inc (:nested-right-brackets app-state))))
-                                              ;; continue looping 
-                                              (recur (assoc app-state 
-                                                            :reader-position (dec (:reader-position app-state))
-                                                            :movement :looping-backward))))
-            (= \+ current-symbol) (recur (assoc app-state
-                                                :reader-position (inc (:reader-position app-state))
-                                                :cells (assoc (:cells app-state) (:cell-pointer app-state) (inc (nth (:cells app-state) (:cell-pointer app-state)))) 
-                                                :movement :moving-forward))
-            (= \- current-symbol) (recur (assoc app-state
-                                                :reader-position (inc (:reader-position app-state))
-                                                :cells (assoc (:cells app-state) (:cell-pointer app-state) (dec (nth (:cells app-state) (:cell-pointer app-state)))) 
-                                                :movement :moving-forward))
-            (= \> current-symbol) (recur (assoc app-state 
-                                                :reader-position (inc (:reader-position app-state))
-                                                :cell-pointer (inc (:cell-pointer app-state))
-                                                :movement :moving-forward))
-            (= \< current-symbol) (recur (assoc app-state
-                                                :reader-position (inc (:reader-position app-state))
-                                                :cell-pointer (dec (:cell-pointer app-state))
-                                                :movement :moving-forward))
-            (= \. current-symbol) (do (print (char (nth (:cells app-state) (:cell-pointer app-state))))
-                                      (recur (assoc app-state
-                                                    :reader-position (inc (:reader-position app-state))
-                                                    :movement :moving-forward)))
-            (= \[ current-symbol) (if (= 0 (nth (:cells app-state) (:cell-pointer app-state))) 
-                                    (recur (assoc app-state
-                                                  :reader-position (inc (:reader-position app-state))
-                                                  :movement :looping-forward))
-                                    (recur (assoc app-state
-                                                  :reader-position (inc (:reader-position app-state))
-                                                  :movement :moving-forward)))
-            (= \] current-symbol) (if (not (= 0 (nth (:cells app-state) (:cell-pointer app-state))))
-                                    (recur (assoc app-state
-                                                  :reader-position (dec (:reader-position app-state))
-                                                  :movement :looping-backward))
-                                    (recur (assoc app-state
-                                                  :reader-position (inc (:reader-position app-state))
-                                                  :movement :moving-forward)))
-            (= \, current-symbol) (recur (assoc app-state
-                                                :reader-position (inc (:reader-position app-state))
-                                                :cells (assoc (:cells app-state) (:cell-pointer app-state) (int (first (read-line))))
-                                                :movement :moving-forward))
-            :else (recur (assoc app-state
-                                :reader-position (inc (:reader-position app-state))
-                                :movement :moving-forward)))))))) ;; Moves reader-position forward if the current character is unknown
+      (if (not (nil? (:terminated-due-to app-state)))
+        app-state
+        (recur (step app-state))))))
 
 (defn -main
   "Time to interpret some Brainfuck"
